4401473c9d7dc81a702f77a3e3f87c5503e1c9f5
Mohit <mohitbro96@gmail.com>
2022-11-13 13:44:26 +0530

    added change package method

--- lib/helper.dart
+++ lib/helper.dart
@@ -73,8 +73,10 @@ abstract class Helper {
 
   static void addPackageSection(List<String> packageSections) {
     try {
-      final packages = packageSections.map((e) => "$e\n").join();
       final contents = pubspecFile.readAsStringSync();
+      if (contents.contains("flutter_icons")) return;
+
+      final packages = packageSections.map((e) => "$e\n").join();
       pubspecFile.writeAsStringSync('$contents\n$packages');
     } catch (e) {
       print("Error occured while adding package: ${e.toString()}");
@@ -90,41 +92,12 @@ abstract class Helper {
     }
   }
 
-  static void clearYAMLComment() async {
-    try {
-      pubspecFile.writeAsStringSync(
-          pubspecFile.readAsStringSync().replaceAll(RegExp("/*#.*"), ""));
-    } catch (e) {
-      print("Error occured while running command : ${e.toString()}");
-    }
-  }
-
   static void runPubGet() async {
     try {
       await Process.run("flutter pub get", [], runInShell: true);
     } catch (e) {
       print("Error occured while enabling assets : ${e.toString()}");
     }
-  }
-
-  static void changeAppName(String appName) {
-    final manifestData = manifestFile.readAsLinesSync();
-    final androidLabel = manifestData
-        .where((element) => element.contains("android:label"))
-        .toList();
-
-    if (androidLabel.isEmpty) {
-      throw Exception("Could not find 'android:label' in ${manifestFile.path}");
-    }
-
-    print(
-        "Old app name : ${androidLabel.first.trim().split("=").last.replaceAll('\"', "")}");
-
-    manifestFile.writeAsStringSync(manifestFile
-        .readAsStringSync()
-        .replaceAll(androidLabel.first, 'android:label="$appName"'));
-
-    print("New app name : $appName");
   }
 
   static void renameProject() {
@@ -144,17 +117,41 @@ abstract class Helper {
     }
 
     /// Changing the app name in the AndroidManifest.xml file.
-    changeAppName(appName);
+    print("Changing app name..");
+    _changeAppName(appName);
 
     if (package == null) {
       throw Exception(
           "You must set the package name under the '$starterKey' section of your pubspec.yaml file.");
     }
 
-    changePackageName(package);
+    /// Changing the package name
+    print("Changing the package name");
+    _changePackageName(package);
   }
 
-  static void changePackageName(String package) {
-    
+  static void _changeAppName(String appName) {
+    final manifestData = manifestFile.readAsLinesSync();
+    final androidLabel = manifestData
+        .where((element) => element.contains("android:label"))
+        .toList();
+
+    if (androidLabel.isEmpty) {
+      throw Exception("Could not find 'android:label' in ${manifestFile.path}");
+    }
+
+    print(
+        "Old app name : ${androidLabel.first.trim().split("=").last.replaceAll('\"', "")}");
+
+    manifestFile.writeAsStringSync(manifestFile
+        .readAsStringSync()
+        .replaceAll(androidLabel.first, 'android:label="$appName"'));
+
+    print("New app name : $appName");
+  }
+
+  static Future<void> _changePackageName(String package) async {
+    await Helper.runCommand(
+        "flutter pub run change_app_package_name:main", [package]);
   }
 }
